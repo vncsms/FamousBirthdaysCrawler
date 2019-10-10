d62b41503ab202bdf1f136a1e997db7f15277564
Vinicius <vmsabc@gmail.com>
2019-10-10 13:48:20 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(name='famous',
       author='Vinicius Morais',
       author_email='vmsabc@gmail.com',
       license='MIT',
+      python_requires='>=3',
+      install_requires=['beautifulsoup4', 'requests'],
       packages=['famous'],
       zip_safe=False)
